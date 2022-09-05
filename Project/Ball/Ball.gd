5f622be1a7bc0336acf3d91442b6d0a77c771f4a
Cameron Allan <camallan16@gmail.com>
2022-09-05 14:28:02 -0400

    Refactored Ball script to allow for a selected impulse

--- Project/Ball/Ball.gd
+++ Project/Ball/Ball.gd
@@ -1,20 +1,14 @@
 extends RigidBody2D
 
-
-var random = RandomNumberGenerator.new()
-
-func _rand():
-	var _range = 999
-	return (random.randi_range((-_range), (_range)))
-	
-
-# Check for Left mouse click
-func _on_Ball_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT and event.pressed == true:
-			apply_central_impulse( Vector2(_rand(), _rand()))
-			print(_rand(), " - ", _rand())
-
-func _process(delta):
+func _ready():
 	pass
+
+func launch(x, y, force):
+	apply_impulse(Vector2.ZERO, Vector2(x * force, y * force))
+	
+func _on_RigidBody2D_body_entered(body):
+	queue_free()
+	if body.has_method("hit"):
+		body.hit()
+
 
